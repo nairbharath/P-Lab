cf2a5904ad034fb026606e10b63e4d67a073b5b0
nairbharath <93637795+nairbharath@users.noreply.github.com>
2021-11-03 12:00:56 +0530

    Program to find area and perimeter of circle

---
+++ P1.py
@@ -0,0 +1,9 @@
+#WAP to find area and perimeter of circle
+import math
+r=float(input("Enter the radius of circle: "))
+pi=math.pi
+area=pi*r*r
+peri=2*pi*r
+print("Area of circle: ",area," Perimeter: ",peri)
+
+
