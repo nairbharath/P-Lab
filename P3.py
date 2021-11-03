e36a7fd92bd960f4b460cbe79c3a144d1b896de7
nairbharath <93637795+nairbharath@users.noreply.github.com>
2021-11-03 15:00:38 +0530

    Swap two variables w/o using temp variable

---
+++ P3.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+print("After Swapping: \nFirst Number:",num1, "\nSecond Number:",num2)
